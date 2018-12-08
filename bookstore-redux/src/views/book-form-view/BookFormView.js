0aac37fde99080df33ee56502e51de457f6306ba
francisco-pinchentti <francisco.pinchentti@gmail.com>
2018-12-08 15:04:42 -0300

    updated form styles to use flex

--- bookstore-redux/src/views/book-form-view/BookFormView.js
+++ bookstore-redux/src/views/book-form-view/BookFormView.js
@@ -5,12 +5,10 @@ export default class BooksFormView extends Component {
 
     render() {
         return (
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-xl-4 col-md-6 col-sm-8 col-12">
-                        <h3>Add a new book</h3>
-                        <BookForm history={this.props.history} />
-                    </div>
+            <div className="d-flex form-container">
+                <div className="form-wrapper">
+                    <h3>Add a new book</h3>
+                    <BookForm history={this.props.history} />
                 </div>
             </div>
         );
--- bookstore-ts/src/views/book-form-view/BookFormView.tsx
+++ bookstore-ts/src/views/book-form-view/BookFormView.tsx
@@ -9,12 +9,10 @@ export default class BooksFormView extends React.Component<BooksFormViewProps> {
 
     public render() {
         return (
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-xl-4 col-md-6 col-sm-8 col-12">
-                        <h3>Add a new book</h3>
-                        <BookForm history={this.props.history} />
-                    </div>
+            <div className="d-flex form-container">
+                <div className="form-wrapper">
+                    <h3>Add a new book</h3>
+                    <BookForm history={this.props.history} />
                 </div>
             </div>
         );
